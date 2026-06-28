5c121ed57b0694bf64bb9c2ddc76632c1c63dff0
Rohit Khandelwal <rohitkhandelwal2001@gmail.com>
2026-06-28 22:10:50 +0530

    simple mode, harvard plate, insights, analytics, sitemap fix, mobile

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   output: 'static',
   site: 'https://caloriemetric.com',
-  integrations: [sitemap()],
 });
